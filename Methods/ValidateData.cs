0c1c79e09b06bc5bd4e138a3c4ef867160b29d7f
andreastollmar <115992410+andreastollmar@users.noreply.github.com>
2023-02-07 11:00:07 +0100

    testade men funka inte

--- Methods/ValidateData.cs
+++ Methods/ValidateData.cs
@@ -2,11 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace WeatherApp.Methods
 {
     internal class ValidateData
     {
+        public static void ValidateDate(string promt)
+        {
+            Console.WriteLine(promt);
+            var input = Console.ReadLine();
+            while (!IsValidDate(input))
+            {
+                Console.WriteLine("Invalid input, try again: ");
+                input = Console.ReadLine();
+            }
+            // Kör nästa method som letar upp alla matches med datumet inläst från användare
+
+        }
+
+        private static bool IsValidDate(string input)
+        {
+            string pattern = @"2016-(0?[0-9]|1?[1-2])-(0-2[0-9]|3[0-1])";
+            return Regex.IsMatch(input, pattern);
+
+        }
     }
 }
---
+++ Models/Day.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Models
+{
+    internal class Day
+    {
+        public int DayNumber { get; set; }
+        public double AvgTemp { get; set; }
+        public double AvgHumidity { get; set; }
+        public int HighestTemp { get; set; }
+        public int LowestTemp { get; set; }
+        public int DayCounter { get; set; }
+
+    }
+}
